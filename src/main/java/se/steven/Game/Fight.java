1668b9e19c406b263de8c3639e3703115b09621c
Steven James Tulk <stevenjatulk@gmail.com>
2024-11-05 15:22:00 +0100

    Began implementation of fighting mechanics

--- src/main/java/se/steven/Game/Fight.java
+++ src/main/java/se/steven/Game/Fight.java
@@ -3,29 +3,35 @@ import se.steven.Models.Entity;
 import se.steven.Models.Resident;
 import se.steven.Models.Burglar;
 import java.util.Scanner;
+import static se.steven.Game.Game.LivingRoom;
 
 public class Fight {
 
+    private static boolean running = true;
     Fight fight = new Fight();
-    Scanner sc = new Scanner(System.in);
+    static Scanner sc = new Scanner(System.in);
 
     public static void battleLogic(Burglar attacker, Resident defender) {
 
+        while (running && attacker.isConcious() && defender.isConcious()) {
             attacker.punch(defender);
-            System.out.println("The burglar takes a swing at you for " + attacker.getDamage() + " damage");
+            System.out.println("The burglar catches you by surprise and sucker punches you for  " + attacker.getDamage() + " damage");
             System.out.println("You now have " + defender.getHealth() + " health left.");
 
-            defender.punch(attacker);
-            System.out.println("You retaliate and hit the burglar for " + defender.getDamage() + " damage");
-            System.out.println("The attacker now has " + attacker.getHealth() + " health left.");
+            if(!defender.isConcious()) {
+                System.out.println("You are no longer able to fight.");
+                running = false;
+            }
 
-            if (attacker.isConcious()) {
-                System.out.println("The burglar is still conscious and swings a right hook. ");
-                attacker.punch(defender);
-            } else {
-                System.out.println("You have knocked out the burglar and can now proceed to the office! ");
+            defender.punch(attacker);
+            System.out.println("You hit back for  " + defender.getDamage() + " damage");
+            System.out.println("The thief now has " + attacker.getHealth() + " health left.");
+
+            if (!attacker.isConcious()) {
+                System.out.println(attacker.getRole() + " has been knocked out!");
+                System.out.println("You have knocked out the burglar and may now proceed to the office!");
+            }
 
             }
         }
     }
-
